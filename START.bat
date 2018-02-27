b6223223320d9a073a6e64f6ec5e6f36ee2bb2f7
Squatty <27901969+VukaKun@users.noreply.github.com>
2018-02-27 20:00:44 +0100

    Update START.bat

--- START.bat
+++ START.bat
@@ -5,6 +5,6 @@ setx GPU_MAX_HEAP_SIZE 100
 setx GPU_USE_SYNC_OBJECTS 1
 setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
-powershell -version 5.0 -noexit -executionpolicy bypass -command "&.\Megaminer.ps1 
+powershell -version 5.0 -noexit -executionpolicy bypass -command "&.\No-BS-Miner.ps1 
 
 
